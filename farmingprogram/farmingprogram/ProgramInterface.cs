dcb4ec90d46bae3e708886dda3e77ea3760dc0cb
sp1062 <sp1062@student.anglia.ac.uk>
2014-10-19 04:18:30 +0100

    Program start
    Started the program

---
+++ farmingprogram/farmingprogram/ProgramInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace farmingprogram
+{
+    public partial class ProgramInterface : Form
+    {
+        public ProgramInterface()
+        {
+            Thread timeoutThread = new Thread(new ThreadStart(SplashScreen.startSplashScreen));
+            timeoutThread.Start();
+            Thread.Sleep(SplashScreen.SPLASH_SCREEN_TIMEOUT);
+            InitializeComponent();
+            timeoutThread.Abort();
+        }
+    }
+}
---
+++ farmingprogram/farmingprogram/SplashScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace farmingprogram
+{
+
+    public partial class SplashScreen : Form
+    {        
+        public const int SPLASH_SCREEN_TIMEOUT = 5000;
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine();
+        }
+
+
+        public static void startSplashScreen()
+        {
+            Application.Run(new SplashScreen());
+        }
+    }
+}
